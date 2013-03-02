c9ac4da80f97b88a72b20c5f377906448c4db12f
Herman Singh <kartstig@gmail.com>
2013-03-02 16:55:25 -0500

    added an address to lat_long converter and marker pins

--- Resources/app.js
+++ Resources/app.js
@@ -1,28 +1,76 @@
 var win = Titanium.UI.createWindow();
 
-function getLatLong(address){
-      var geo = new google.maps.Geocoder;
+var addr = "2138 Triad Court";
+var city = "Columbus";
+var state = "OH";
 
-      geo.geocode({'address':address},function(results, status){
-              if (status == google.maps.GeocoderStatus.OK) {
-                return results[0].geometry.location.lattitude;
-                return results[0].geometry.location.longitude;
-              } else {
-                alert("Geocode was not successful for the following reason: " + status);
-              }
+function createPin(address, callback) {
 
-       });
+	var lat_long = [];
 
-  }
- 
-var mapView = Titanium.Map.createView({
-	    mapType : Titanium.Map.STANDARD_TYPE,
-    	region : {latitude:40.001780, longitude:-83.019691,latitudeDelta:0.04, longitudeDelta:0.04},            
-    	animate : true,
-    	regionFit : true,
-    	userLocation : false
-});
- 
-win.add(mapView);
+	var addrsplit = address.split(" ");
+	var citysplit = city.split(" ");
 
-win.open();
+	var addrjoin = addrsplit.join("+");
+	var cityjoin = citysplit.join("+");
+
+	var addrUrl = "http://maps.googleapis.com/maps/api/geocode/json?address=" + addrjoin + "," + cityjoin + "," + state + "&sensor=false";
+
+	Ti.API.info(addrUrl);
+
+	/* web-service call */
+	var addrReq = Titanium.Network.createHTTPClient();
+	addrReq.open("GET", addrUrl);
+	addrReq.send();
+
+	addrReq.onload = function() {
+		var response = JSON.parse(this.responseText);
+		Ti.API.info(response.toString());
+		if (response.status == "OK") {
+			// Debugging
+			Ti.API.info("lat : " + response.results[0].geometry.location.lat);
+			Ti.API.info("lng : " + response.results[0].geometry.location.lng);
+
+			lat_long[0] = response.results[0].geometry.location.lat;
+			lat_long[1] = response.results[0].geometry.location.lng;
+
+			callback(Titanium.Map.createAnnotation({
+				title : "Parking Spots",
+				pincolor : Titanium.Map.ANNOTATION_RED,
+				animate : true,
+				latitude : lat_long[0],
+				longitude : lat_long[1],
+
+			}));
+
+			//return lat_long;
+		} else {
+			showAlert('', 'Unable to find Address');
+		}
+	};
+
+}
+
+function display_annotation(anno) {
+	//Ti.API.info(anno);
+
+	var mapView = Titanium.Map.createView({
+		mapType : Titanium.Map.STANDARD_TYPE,
+		region : {
+			latitude : 40.001780,
+			longitude : -83.019691,
+			latitudeDelta : 0.04,
+			longitudeDelta : 0.04
+		},
+		animate : true,
+		regionFit : true,
+		userLocation : false,
+		annotations : [anno],
+	});
+
+	win.add(mapView);
+
+	win.open();
+}
+
+var pins = [createPin(addr, display_annotation)];
